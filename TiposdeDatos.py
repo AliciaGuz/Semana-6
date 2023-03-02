094076e9bca98f81f4d697f9482535e66bdad74b
AliciaGuz <126368097+AliciaGuz@users.noreply.github.com>
2023-03-02 14:44:59 -0600

    Add files via upload

---
+++ TiposdeDatos.py
@@ -0,0 +1,26 @@
+#Tipos de Datos enteros (int) y largos (long)
+numeroEntero = 9
+#print("El valor de la variable numeroEntero es: ",numeroEntero, "y su tipo de dato es: ",type (numeroEntero))
+numeroLargo= 9
+#print ("el valor de la variable numeroLargo es: ",numeroLargo, "y su tipo de dato es: ",type (numeroLargo))
+
+#Tipos de datos float
+numeroFlotante= 5.66
+print(numeroFlotante, type(numeroFlotante))
+
+#Tipos de datos Complejos
+numeroComplejos= 5 + 5j 
+print(numeroComplejos, type(numeroComplejos))
+
+#Tipos de datos string
+nombre1= "Aaron"
+nombre2= "Geime"
+nombre3= """Aliciaç
+Estefany
+Guzmàn
+Lòpez"""
+nombre4= "Edgar Rene \n Rivas Castro"
+print(nombre1, type(nombre1))
+print(nombre2,type(nombre2))
+print(nombre3,type(nombre3))
+print(nombre4, type(nombre4))
